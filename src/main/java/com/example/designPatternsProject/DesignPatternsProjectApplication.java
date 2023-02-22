68ff0e6f6a73feafcaa9a20e7babb0e3110492b7
Mariana <frunzem7@gmail.com>
2023-02-22 09:53:29 +0200

    First commit

---
+++ src/main/java/com/example/designPatternsProject/DesignPatternsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.designPatternsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/designPatternsProject/DesignPatternsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designPatternsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
